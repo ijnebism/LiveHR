bad07d46e37c4105a9740762b34f84eb1fda996b
ijnebism <bjqian0201@gmail.com>
2026-09-09 21:32:41 +1200

    feat: obtain advertising hr sensors

--- LiveHR.cpp
+++ LiveHR.cpp
@@ -1,9 +1,31 @@
-// LiveHR.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <winrt/windows.devices.bluetooth.h>
+#include <winrt/windows.devices.bluetooth.advertisement.h>
+#include <winrt/windows.devices.enumeration.h>
+#include <winrt/Windows.Foundation.Collections.h>
+
+using namespace winrt::Windows::Devices::Bluetooth::Advertisement;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	winrt::init_apartment();
+
+    std::cout << "Starting Scan\n";
+	
+	BluetoothLEAdvertisementWatcher watcher;
+	BluetoothLEAdvertisementFilter filter;
+
+	filter.Advertisement().ServiceUuids().Append(winrt::guid(L"0000180D-0000-1000-8000-00805F9B34FB")); // Heart Rate Service UUID
+	watcher.AdvertisementFilter(filter);
+	watcher.ScanningMode(BluetoothLEScanningMode::Active);
+
+	
+	watcher.Received([](BluetoothLEAdvertisementWatcher const& sender, BluetoothLEAdvertisementReceivedEventArgs const& args)
+		{
+			std::wcout << L"Advertisement received from: " << args.BluetoothAddress() << L"\n";
+		});
+
+	watcher.Start();
+	std::cout << "Press Enter to stop scanning...\n";
+	std::cin.get();
 }
